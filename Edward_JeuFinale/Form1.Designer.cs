cdc0f8b984dc0393c21ef9cf5655f27c7822d957
shaigreatness26 <edward.reso@mon-avenir.ca>
2026-06-07 22:46:31 -0400

    Update Form1.Designer.cs

--- Edward_JeuFinale/Form1.Designer.cs
+++ Edward_JeuFinale/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Edward_JeuFinale
+namespace Edward_JeuFinale
 {
     partial class Form1
     {
@@ -36,11 +36,20 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.hoop = new System.Windows.Forms.PictureBox();
             this.background = new System.Windows.Forms.PictureBox();
+            this.rimBounds = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.retroactionTir = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.feedbackTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hoop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rimBounds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -62,8 +71,8 @@
             // 
             // Player
             // 
-            this.Player.BackColor = System.Drawing.Color.Transparent;
-            this.Player.BackgroundImage = global::Edward_JeuFinale.Properties.Resources.greensmall_Jalenbrunsonsprite;
+            this.Player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(92)))));
+            this.Player.BackgroundImage = global::Edward_JeuFinale.Properties.Resources.small_Jalenbrunsonsprite;
             this.Player.Location = new System.Drawing.Point(223, 399);
             this.Player.Name = "Player";
             this.Player.Size = new System.Drawing.Size(60, 74);
@@ -100,13 +109,71 @@
             this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.background.TabIndex = 0;
             this.background.TabStop = false;
-            this.background.Click += new System.EventHandler(this.background_Click);
+            // 
+            // rimBounds
+            // 
+            this.rimBounds.Location = new System.Drawing.Point(810, 340);
+            this.rimBounds.Name = "rimBounds";
+            this.rimBounds.Size = new System.Drawing.Size(61, 20);
+            this.rimBounds.TabIndex = 5;
+            this.rimBounds.TabStop = false;
+            this.rimBounds.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(561, 479);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 58);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(92)))));
+            this.groupBox1.Controls.Add(this.retroactionTir);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(484, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 86);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // retroactionTir
+            // 
+            this.retroactionTir.AutoSize = true;
+            this.retroactionTir.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retroactionTir.ForeColor = System.Drawing.Color.Green;
+            this.retroactionTir.Location = new System.Drawing.Point(52, 42);
+            this.retroactionTir.Name = "retroactionTir";
+            this.retroactionTir.Size = new System.Drawing.Size(77, 23);
+            this.retroactionTir.TabIndex = 1;
+            this.retroactionTir.Text = "PARFAIT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Retroaction de tir:";
+            // 
+            // feedbackTimer
+            // 
+            this.feedbackTimer.Interval = 1600;
+            this.feedbackTimer.Tick += new System.EventHandler(this.feedbackTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rimBounds);
             this.Controls.Add(this.ball);
             this.Controls.Add(this.Player);
             this.Controls.Add(this.pictureBox2);
@@ -116,13 +183,16 @@
             this.Name = "Form1";
             this.Text = "Jeu Basket";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.hoop)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rimBounds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,6 +206,12 @@
         private System.Windows.Forms.PictureBox Player;
         private System.Windows.Forms.PictureBox ball;
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox rimBounds;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label retroactionTir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer feedbackTimer;
     }
 }
 
